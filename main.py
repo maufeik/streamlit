500409ccad57428e56098183884463ed7c4e25c2
maufeik <mautriveno@gmail.com>
2025-09-22 19:08:07 +0200

    fix errors

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 
 weather = st.Page("weather.py", title="Weather", icon=":material/add_circle:")
-penguins = st.Page("burtin.py", title="Penguin", icon=":material/add_circle:")
+burtin = st.Page("burtin.py", title="Penguin", icon=":material/add_circle:")
 
-pg = st.navigation([weather,penguins])
+pg = st.navigation([weather,burtin])
 st.set_page_config(
     page_title="Prueba",
     page_icon="𝚾",
